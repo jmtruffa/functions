08b795ac7e3f9d34e676beea0b33263a2f10f914
juan <>
2022-02-04 18:59:30 -0300

    accumPeriodicMultiple created for multiple tickers

--- acumPeriodic.R
+++ acumPeriodic.R
@@ -38,6 +38,52 @@ acumPeriodic = function(ticker = "QQQ",
   return(res)
 }
 
+accumPeriodicMultiple = function (tickers,
+                                  amountInvest,
+                                  start,
+                                  end,
+                                  accum = FALSE
+) 
+{
+  if (accum == TRUE) {
+    full = data.frame(
+      ticker = character(),
+      date = Date(),
+      accum = numeric()
+    )
+    names = c('ticker', 'date', 'accum')
+  } else {
+    full = data.frame(
+      ticker = character(),
+      amount = numeric()
+    )
+    names = c('ticker', 'amount')
+  }
+  
+  for (company in tickers) {
+    r = acumPeriodic(company, 
+                     amountInvest = amountInvest,
+                     start = start,
+                     end = end,
+                     accum = accum)
+    r = cbind(rep(company,length(r)), r )
+    full = rbind(full, r)  
+  }
+  colnames(full) = names
+  return(full)
+}
+
+############# acumPeriodcMultiple
+############# Wrapper de accumPeriodic pero recibe un vector para calcular multiples
+############# Devuelve un df con los tickers y sus valores. 
+############# Si accum = TRUE devuelve el df con los valores acumulados para cada período
+test = accumPeriodicMultiple(tickers = c('QQQ', 'SPY', 'INTC', 'MSFT', 'KO', 'X'),
+                             amountInvest = 50,
+                             start = '2001-01-01',
+                             end = '2021-12-31',
+                             accum = TRUE)
+
+full %>% ggplot(aes(x = date, y = accum, color = ticker)) + geom_line()
 
 acumPeriodicOLD = function(ticker = "QQQ",
                         amountInvest = 1,
