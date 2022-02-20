5b6f8a3187e81c5ee6999a89398c4d18bfcc55f9
juan <>
2022-02-20 13:19:43 -0300

    Agregado chequeo validUntil en merval. Simplificada evaluacion en alphacast

--- alphacast.R
+++ alphacast.R
@@ -2,16 +2,14 @@
 # Function to get datasets from Alphacast.io
 # uses alphacast key from .Renviron
 # validUntil = 0 will force re-download.
+# directory = where to store the downloaded file. If there's a data directory in your working folders specify the absolute path here.
+# I leave the default option of an absolute path of myself since that is my data/ directory.
 
-getAlphacast = function(ds, validUntil = 0) {
-  download = FALSE
-  fileName = paste0('/Volumes/GoogleDrive/Mi unidad/analisis financieros/functions/data/', ds, '.csv')
+getAlphacast = function(ds, validUntil = 0, directory = '/Volumes/GoogleDrive/Mi unidad/analisis financieros/functions/data/') {
+  #download = FALSE
+  fileName = paste0(directory, ds, '.csv')
 
   if (!file.exists(fileName) || (file.info(fileName)$ctime + (validUntil * 60) < Sys.time())) {
-    download = TRUE
-  }
-
-  if (download == TRUE) {
     download.file(paste0('https://api.alphacast.io/datasets/',ds,'/data?apiKey=', Sys.getenv("alphacast"),'&&format=csv' ),
                   fileName,
                   sep='',
@@ -19,6 +17,7 @@ getAlphacast = function(ds, validUntil = 0) {
   }
   read.csv(fileName)
 }
+
 
 getAlphacastDataSets = function() {
   r = httr::GET('https://charts.alphacast.io/api/datasets',
