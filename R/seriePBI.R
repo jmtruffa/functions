41faee09b97357eece84fd89c738fe403639d7c2
juan <>
2023-05-04 09:50:17 -0300

    agregado getEMAE

--- R/seriePBI.R
+++ R/seriePBI.R
@@ -120,8 +120,59 @@ downloadPBI = function(db = "", url = "https://www.indec.gob.ar/ftp/cuadros/econ
       ret(error)
     }
 
-    DBI::dbDisconnect(con)
-    return(ret)
+
+  ### Ahora el EMAE
+  url = "https://www.indec.gob.ar/ftp/cuadros/economia/sh_emae_mensual_base2004.xls"
+  tmpFileName = "emae.xls"
+
+  set.seed(1973)
+  if (file.exists(file.path(tmpPath, tmpFileName))) {
+    tempN=round(runif(1)*10)
+    file.copy(file.path(tmpPath, tmpFileName), file.path(tmpPath, paste0('tmp', tempN, tmpFileName)), overwrite = TRUE)
+  }
+
+  tryCatch(
+    {
+      download.file(url, destfile = file.path(tmpPath, tmpFileName))
+    },
+    error = function(e) {
+      error <<- TRUE;
+      e <<- e;
+    }
+  )
+
+  if (!error) {
+
+    emae = readxl::read_xls(file.path(tmpPath, tmpFileName),
+                                    sheet = "emae",
+                                    skip = 4,
+                            col_names = F)
+    t = emae[-c(nrow(emae), nrow(emae)-1),]
+    t = as.data.frame(as.xts(ts(t[,-c(1:2)], start = c(2004, 1), frequency = 12)))
+    t$date = rownames(t)
+    t = t %>% relocate(date)
+    colnames(t) = c(
+      "date",
+      "emae",
+      "emaVarMismoPeriodo",
+      "emaDesestacionalizado",
+      "emaeDesestacinalizadoVarMismoPeriodo",
+      "emaeTendenciaCiclo",
+      "emaeTendenciaCicloVarMismoPeriodo"
+    )
+    rownames(t) = NULL
+
+
+  } else {
+    ret(error)
+  }
+
+  DBI::dbWriteTable(con, "emae", t, overwrite = TRUE)
+
+  ### Termino cerrando la conexión a la DB
+  DBI::dbDisconnect(con)
+  return(ret)
+
 
 
 }
@@ -130,6 +181,7 @@ getPBI = function(table = "pbiCorriente", format = "T", db= "") {
 
   require(RSQLite)
   require(DBI)
+  require(stringr)
 
 
 
@@ -164,6 +216,30 @@ getPBI = function(table = "pbiCorriente", format = "T", db= "") {
 
   DBI::dbDisconnect(con)
   return(pbi)
+}
+
+getEMAE = function(db= "") {
+
+  require(RSQLite)
+  require(DBI)
+  require(stringr)
+
+
+
+  if (db == "") {
+    if (str_detect(Sys.info()['nodename'], "Air")) {
+      db = "~/data/data1.sqlite3"
+    } else {
+      db = '~/data/data1.sqlite3'
+    }
+  }
+
+  con = DBI::dbConnect(RSQLite::SQLite(), dbname = db)
+
+  emae = DBI::dbReadTable(con, "emae")
+
+  DBI::dbDisconnect(con)
+  return(emae)
 }
 
 
