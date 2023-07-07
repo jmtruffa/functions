06f1119bc9e05ea52d1c9e1e58501fc229192fed
juan <>
2023-07-07 13:57:17 -0300

    agregada getPrices y modificada getYields

--- R/getPrices.R
+++ R/getPrices.R
@@ -1,34 +1,37 @@
-#' getYieds
+#' getPrices
 #'
-#' @details Function to get yields from the local server running yields.go at 127.0.0.1:8080.
-#' Fees are recycled for all the bonds in the array.
+#' @details
+#'
+#' Obtiene el precio, dada una tasa, de un activo. Opera contra la API local de yields.
+#' Pueden pasársele vectores. Fees y settlement no opera con vectores.
+#'
 #' @param letras Puede ser un vector conteniendo múltiples
 #' @param settlementDate Fecha en formato "yyyy-mm-dd"
-#' @param precios Puede ser vector.
+#' @param yield Puede ser vector.
 #' @param initialFee Valor único a aplicar upfront. Se recicla
 #' @param endingFee Valor único a aplicar al final (si se intenta estimar una venta posterio)
-#' @param endpoint "yield" o "apr". Dependiendo a qué endpoint del servidor quiera pegársele.
+#' @param endpoint "price"
+#' @examples getPrices("GD30D", "2023-07-13", 0.28, initialFee = 0.007515, endingFee = 0, endpoint = "yield")
 #'
-#' @examples getYields("GD30D", "2023-07-13", 32.3, initialFee = 0.007515, endingFee = 0, endpoint = "yield")
-#'
-#' @return tibble with same data plus yield & duration. There are several other values that the API
+#' @return tibble with same data plus price & mduration. There are several other values that the API
 #' returns but not implemented yet.
 
-getYields = function(letras, settlementDate, precios, initialFee = 0, endingFee = 0, endpoint = "yield") {
+
+
+getPrices = function(letras, settlementDate, yield, initialFee = 0, endingFee = 0, endpoint = "price") {
 
   require(httr)
   require(tidyverse)
-  require(functions)
 
   initialFee = rep(initialFee, length(letras))
   endingFee = rep(endingFee, length(letras))
-  yield = rep(0, length(letras))
+  prices = rep(0, length(letras))
   mduration = rep(0, length(letras))
 
 
   result = tibble(
     letras,
-    precios,
+    prices,
     initialFee,
     endingFee,
     yield,
@@ -39,21 +42,19 @@ getYields = function(letras, settlementDate, precios, initialFee = 0, endingFee 
 
   for (i in 1:length(letras)) {
 
-      r = GET(url,
+    r = GET(url,
             query = list(
               ticker = result$letras[i],
               settlementDate = settlementDate[i],
-              price = result$precios[i],
+              rate = result$yield[i],
               initialFee = result$initialFee[i],
               endingFee =  result$endingFee[i]
             )
-          )
+    )
 
-    result$yield[i] = as.data.frame(fromJSON(rawToChar(r$content)))$Yield
+    result$prices[i] = as.data.frame(fromJSON(rawToChar(r$content)))$Price
     result$mduration[i] = as.data.frame(fromJSON(rawToChar(r$content)))$MDuration
     i=i+1
   }
   result
 }
-
-
