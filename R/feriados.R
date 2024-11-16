44a0c73c1f9508080e8de94507a3033e75688669
juan <>
2024-11-16 11:08:28 -0300

    feriados ahora apunta a postgres

--- R/feriados.R
+++ R/feriados.R
@@ -1,66 +1,24 @@
-getFeriados = function(db = "") {
-  require(stringr)
-  require(RSQLite)
-  if (db == "") {
-    if (str_detect(Sys.info()['nodename'], "Air")) {
-      db = "~/data/test.sqlite3"
-    } else {
-      db = '~/data/test.sqlite3'
-    }
-  }
-  con = DBI::dbConnect(RSQLite::SQLite(), dbname = db)
-  feriados = DBI::dbReadTable(con, "calendarioFeriados")
-  DBI::dbDisconnect(con)
-  as.Date(feriados$date)
+getFeriados = function(...) {
+  dbGetTable("calendarioFeriados", server = server, port = port)$date
 }
 
 
 addFeriados = function(
     lista,
-    db = "") {
-  require(stringr)
-  require(RSQLite)
-  if (db == "") {
-    if (str_detect(Sys.info()['nodename'], "Air")) {
-      db = "~/data/test.sqlite3"
-    } else {
-      db = '~/data/test.sqlite3'
-    }
-  }
-  con = dbConnect(RSQLite::SQLite(), dbname = db)
-  feriados = dbReadTable(con, "calendarioFeriados")
-  feriados$date = as.Date(feriados$date)
-
+    ...) {
+  
+  feriados = dbGetTable("calendarioFeriados", server = server, port = port)
   juntos = as_tibble(rbind(feriados, tibble(date = lista)))
   juntos = juntos %>% distinct() %>% arrange(date)  #remueve duplicados y ordena
-  juntos$date = as.character(juntos$date) #convierte a texto para mandar a db
-
-  DBI::dbWriteTable(con, "calendarioFeriados", juntos, overwrite = TRUE)
-  DBI::dbDisconnect(con)
+  dbWriteDF(table = "calendarioFeriados", df = juntos, server = server, port = port, overwrite = T)
 }
 
-
 removeFeriados = function(
-    lista,
-    db = "") {
-  require(stringr)
-  require(RSQLite)
-  if (db == "") {
-    if (str_detect(Sys.info()['nodename'], "Air")) {
-      db = "~/data/test.sqlite3"
-    } else {
-      db = '~/data/test.sqlite3'
-    }
-  }
-  con = dbConnect(RSQLite::SQLite(), dbname = db)
-  feriados = dbReadTable(con, "calendarioFeriados")
-  feriados$date = as.Date(feriados$date)
-
+    lista, ...) {
+  
+  feriados = dbGetTable("calendarioFeriados", server = server, port = port)
   feriados = feriados %>% filter(!date %in% as.Date(lista))
-  feriados$date = as.character(feriados$date)
-
-  DBI::dbWriteTable(con, "calendarioFeriados", feriados, overwrite = TRUE)
-  DBI::dbDisconnect(con)
+  dbWriteDF(table = "calendarioFeriados", df = feriados, server = server, port = port, overwrite = T)
 
 }
 
